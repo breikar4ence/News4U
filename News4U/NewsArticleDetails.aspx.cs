29b671f97c649c24b8d8f062252f7ce72599c3ca
breikar4ence <breikar4ence@abv.bg>
2016-07-23 18:02:41 +0100

    Revert "Working but details always shows same article"
    This reverts commit a350ce1c83df80d35b6beeab3f1ba9961641d10c.

--- News4U/NewsArticleDetails.aspx.cs
+++ News4U/NewsArticleDetails.aspx.cs
@@ -1,9 +1,7 @@
-﻿using News4U.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.ModelBinding;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -14,17 +12,6 @@ namespace News4U
         protected void Page_Load(object sender, EventArgs e)
         {
 
-        }
-
-        public IQueryable<NewsArticle> GetNewsArticle([QueryString("id")] int? NewsArticleId)
-        {
-            var _db = new News4U.Models.NewsArticleContext();
-            IQueryable<NewsArticle> query = _db.NewsArticle;
-            if (NewsArticleId.HasValue && NewsArticleId > 0)
-            {
-                query = query.Where(p => p.NewsArticleID == NewsArticleId);
-            }
-            return query;
         }
     }
 }
