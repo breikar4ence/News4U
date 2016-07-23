9146d9f919bf4debfce4825e9c41cdae35599b57
breikar4ence <breikar4ence@abv.bg>
2016-07-23 19:59:47 +0100

    Working

---
+++ News4U/Logic/AddNewsArticles.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using News4U.Models;
+
+namespace News4U.Logic
+{
+    public class AddNewsArticles
+    {
+        public bool AddNewsArticle(string NewsArticleTitle, string NewsArticleMainBody, string NewsArticlePrice, string NewsArticleCategory, string NewsArticleImagePath)
+        {
+            var myNewsArticle = new NewsArticle();
+            myNewsArticle.NewsArticleTitle = NewsArticleTitle;
+            myNewsArticle.NewsArticleMainBody = NewsArticleMainBody;
+            myNewsArticle.NewsArticleImagePath = NewsArticleImagePath;
+
+            using (NewsArticleContext _db = new NewsArticleContext())
+            {
+                // Add NewsArticle to DB.
+                _db.NewsArticle.Add(myNewsArticle);
+                _db.SaveChanges();
+            }
+            // Success.
+            return true;
+        }
+    }
+}
