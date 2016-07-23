9184a65a58fd18f3ba8d7b24b188a2fe385c1bb3
breikar4ence <breikar4ence@abv.bg>
2016-07-23 18:47:24 +0100

    Revert "YES WORKING DETAILS PAGE"
    This reverts commit 2270527d0acca9b85dd4ac7d6103bc9035f4843c.

--- News4U/NewsArticleDetails.aspx.cs
+++ News4U/NewsArticleDetails.aspx.cs
@@ -1,9 +1,7 @@
-﻿using News4U.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.ModelBinding;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -14,21 +12,6 @@ namespace News4U
         protected void Page_Load(object sender, EventArgs e)
         {
 
-        }
-
-        public IQueryable<NewsArticle> GetArticles([QueryString("NewsArticleID")] int? NewsArticleID)
-        {
-            var _db = new News4U.Models.NewsArticleContext();
-            IQueryable<NewsArticle> query = _db.NewsArticle;
-            if (NewsArticleID.HasValue && NewsArticleID > 0)
-            {
-                query = query.Where(p => p.NewsArticleID == NewsArticleID);
-            }
-            else
-            {
-                query = null;
-            }
-            return query;
         }
     }
 }
