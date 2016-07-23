8d39522c2def0c2add59026f3b9b77a56c2c4507
breikar4ence <breikar4ence@abv.bg>
2016-07-23 17:09:22 +0100

    Working

---
+++ News4U/NewsArticleDetails.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="NewsArticleDetails.aspx.cs" Inherits="News4U.NewsArticleDetails" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
--- News4U/NewsArticleDetails.aspx.cs
+++ News4U/NewsArticleDetails.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace News4U
 {
-    public partial class NewsArtileDetails : System.Web.UI.Page
+    public partial class NewsArticleDetails : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
