f3345289c8a258a4411448ef12d4026989d546c4
breikar4ence <breikar4ence@abv.bg>
2016-07-24 02:48:09 +0100

    FInal final

--- News4U/NewsArticleList.aspx.designer.cs
+++ News4U/NewsArticleList.aspx.designer.cs
@@ -19,6 +19,6 @@ namespace News4U {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ListView NewsArticleList;
+        protected global::System.Web.UI.WebControls.ListView NewsArticle;
     }
 }
